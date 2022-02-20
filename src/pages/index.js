147728356eddb34be9aed5de7a3b034389790d05
sava <savamedvedev@gmail.com>
2022-02-20 10:38:06 +0800

    therd commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
-
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
-  console.log(countries);
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.common.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) 
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className={styles.inputContainer}>
         <div className={styles.counts}>Found {countries.length} countries</div>
         <div className={styles.input}>
-          <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+          <SearchInput
+            placeholder="Filter by Name, Region or SubRegion"
+            onChange={onInputChange}
+          />
         </div>
       </div>
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
